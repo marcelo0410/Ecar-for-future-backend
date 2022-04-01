2f7898f1b6f2391f9477fd9f4ae3d5e25c33dc66
marcelo0410 <cs95401156@gmail.com>
2022-04-01 22:15:47 +0800

    Test controller

--- src/main/java/com/noma/ecarforfuture/EcarForFutureApplication.java
+++ src/main/java/com/noma/ecarforfuture/EcarForFutureApplication.java
@@ -1,13 +1,14 @@
 package com.noma.ecarforfuture;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EcarForFutureApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcarForFutureApplication.class, args);
     }
-
 }
