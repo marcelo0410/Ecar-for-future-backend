80e301d9e27bc70aeca7139aa8a2d1df413299e9
marcelo0410 <cs95401156@gmail.com>
2022-04-01 21:18:51 +0800

    initial commit

---
+++ src/main/java/com/noma/ecarforfuture/EcarForFutureApplication.java
@@ -0,0 +1,13 @@
+package com.noma.ecarforfuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcarForFutureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcarForFutureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noma/ecarforfuture/EcarForFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noma.ecarforfuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcarForFutureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
